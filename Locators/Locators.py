5392907143a99e7910341cf0393c9f45ed1c7a56
Lewis O'Brien <>
2022-10-17 11:31:36 +1100

    Element locators

---
+++ Locators/Locators.py
@@ -0,0 +1,22 @@
+# Login page
+txt_username = "id=user-name"
+txt_password = "id=password"
+btn_login = "id=login-button"
+
+# Products page
+lnk_shopping_cart = "id=shopping_cart_container"
+
+# Cart page
+btn_checkout = "id=checkout"
+
+# Checkout step one page
+txt_first_name = "id=first-name"
+txt_last_name = "id=last-name"
+txt_zip_code_name = "id=postal-code"
+btn_continue = "id=continue"
+
+# Checkout step two page
+btn_finish = "id=finish"
+
+# Checkout complete page
+img_pony_express = ".pony_express"
